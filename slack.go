c309854f140dae5d847a669393e7ec7a0e595191
Alexander F. Rødseth <alexander.fet.rodseth@schibsted.com>
2023-03-29 09:39:49 +0200

    Add two source files

---
+++ slack.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/slack-go/slack"
+)
+
+type SlackClient struct {
+	client  *slack.Client
+	channel string
+}
+
+func NewSlackClient(config *Config) *SlackClient {
+	return &SlackClient{
+		client:  slack.New(config.SlackToken),
+		channel: config.SlackChannel,
+	}
+}
+
+func (sc *SlackClient) SendMessage(channel, message string) error {
+	if channel == "" {
+		channel = sc.channel
+	}
+
+	_, _, err := sc.client.PostMessage(channel, slack.MsgOptionText(message, false))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sc *SlackClient) SendMeetingConclusion(conclusion string, startTime time.Time, duration time.Duration) error {
+	message := fmt.Sprintf("Meeting conclusion for %s (Duration: %v):\n%s", startTime.Format("2006-01-02 15:04:05"), duration, conclusion)
+	return sc.SendMessage(sc.channel, message)
+}
+
+func (sc *SlackClient) UpdateConfig(token, channel string) {
+	if token != "" {
+		sc.client = slack.New(token)
+	}
+	if channel != "" {
+		sc.channel = channel
+	}
+}
---
+++ text_to_speech.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+	"time"
+)
+
+type TextToSpeech struct {
+	apiKey string
+}
+
+func NewTextToSpeech(config *Config) *TextToSpeech {
+	return &TextToSpeech{
+		apiKey: config.TextToSpeechAPIKey,
+	}
+}
+
+func (tts *TextToSpeech) Speak(text string) error {
+	audioData, err := tts.generateAudio(text)
+	if err != nil {
+		return err
+	}
+
+	tempFileName := fmt.Sprintf("temp_audio_%d.wav", time.Now().UnixNano())
+	err = ioutil.WriteFile(tempFileName, audioData, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = os.Remove(tempFileName)
+	}()
+
+	cmd := exec.Command("afplay", tempFileName)
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tts *TextToSpeech) generateAudio(text string) ([]byte, error) {
+	url := "https://text-to-speech-api.example.com/v1/synthesize"
+
+	data := map[string]string{
+		"text": text,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+tts.apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("Text-to-Speech API request failed with status: " + resp.Status)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bodyBytes, nil
+}
