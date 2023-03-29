b45a561b003b2e6f77ad694f1b53d4b494c7ffe0
Alexander F. Rødseth <alexander.fet.rodseth@schibsted.com>
2023-03-29 16:57:08 +0200

    Let the slack utility read the transcribed text

--- cmd/slackhello/main.go
+++ cmd/slackhello/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	concluder "github.schibsted.io/alexander-fet-rodseth/hackday-meeting-concluder"
 )
 
+const transcriptFile = "input.txt"
+
 func main() {
-	err := concluder.SendMessage("hello")
+	data, err := os.ReadFile(transcriptFile)
 	if err != nil {
+		fmt.Printf("Error reading %s: %v\n", transcriptFile, err)
+		return
+	}
+
+	transcription := string(data)
+	fmt.Printf("Sending:\n%s\n", transcription)
+
+	if err := concluder.SendMessage(transcription); err != nil {
 		fmt.Printf("Error sending message to Slack channel: %v\n", err)
 		return
+
 	}
 	fmt.Println("Message sent to Slack channel successfully.")
 }
