26d848e91903985f7a93e6c7ac4852157c417859
Alexander F. Rødseth <alexander.fet.rodseth@schibsted.com>
2023-03-29 16:13:40 +0200

    Update the Slack utility

--- cmd/slackhello/main.go
+++ cmd/slackhello/main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
 	concluder "github.schibsted.io/alexander-fet-rodseth/hackday-meeting-concluder"
 )
 
 func main() {
-	slackToken := os.Getenv("SLACK_API_KEY")
-	slackChannel := os.Getenv("SLACK_CHANNEL")
+	slackWebhook := concluder.NewSlackWebhook()
 
-	if slackToken == "" || slackChannel == "" {
-		log.Fatal("SLACK_API_KEY and SLACK_CHANNEL environment variables must be set.")
-	}
-
-	slackClient := concluder.NewSlackClient(slackToken, slackChannel)
-
-	err := slackClient.SendMessage(slackChannel, "hello")
+	err := slackWebhook.SendMessage("hello")
 	if err != nil {
 		fmt.Printf("Error sending message to Slack channel: %v\n", err)
 		return
